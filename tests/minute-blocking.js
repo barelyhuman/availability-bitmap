05c7093a4428cb8d60e9948c433c7f57c0503392
Reaper <ahoy@barelyhuman.dev>
2022-03-20 08:57:11 +0530

    add in more tests

--- tests/minute-blocking.js
+++ tests/minute-blocking.js
@@ -36,4 +36,77 @@ test("Basic Overlaping Block over minutes", () => {
   assert.not(blocked2);
 });
 
+test("2 min gap between blocks, min requirement 60 mins", () => {
+  const minRange = new AvailabilityMinuteRange();
+
+  const blocked1 = minRange.blockTime(
+    new Date(new Date().setHours(0, 0, 0, 0)),
+    new Date(new Date().setHours(1, 0, 0, 0))
+  );
+
+  const blocked2 = minRange.blockTime(
+    new Date(new Date().setHours(1, 2, 0, 0)),
+    new Date(new Date().setHours(1, 30, 0, 0))
+  );
+
+  const availability = minRange.nextAvailableFrom(
+    new Date(new Date().setHours(1, 0)),
+    60
+  );
+
+  assert.ok(blocked1);
+  assert.ok(blocked2);
+  assert.ok(availability.availableFor >= 60);
+});
+
+test("blocked completely", () => {
+  const minRange = new AvailabilityMinuteRange();
+
+  const blocked1 = minRange.blockTime(
+    new Date(new Date().setHours(0, 0, 0, 0)),
+    new Date(new Date().setHours(23, 59, 59))
+  );
+
+  const blocked2 = minRange.blockTime(
+    new Date(new Date().setHours(1, 2, 0, 0)),
+    new Date(new Date().setHours(1, 30, 0, 0))
+  );
+
+  const availability = minRange.nextAvailableFrom(
+    new Date(new Date().setHours(1, 0)),
+    60
+  );
+
+  assert.not(availability.availableFor);
+  assert.ok(blocked1);
+  assert.not(blocked2);
+});
+
+test("Multiple Ranges", () => {
+  const hotelOne = new AvailabilityMinuteRange();
+  const hotelTwo = new AvailabilityMinuteRange();
+  let bookedSecond = false;
+
+  // book first one for 2 hours
+  hotelOne.blockTime(
+    new Date(new Date().setHours(0, 0, 0, 0)),
+    new Date(new Date().setHours(2, 0, 0, 0))
+  );
+
+  // book the same hotel for the same time
+  const blocked2 = hotelOne.blockTime(
+    new Date(new Date().setHours(0, 0, 0, 0)),
+    new Date(new Date().setHours(2, 0, 0, 0))
+  );
+
+  if (!blocked2) {
+    bookedSecond = hotelTwo.blockTime(
+      new Date(new Date().setHours(0, 0, 0, 0)),
+      new Date(new Date().setHours(2, 0, 0, 0))
+    );
+  }
+
+  assert.ok(bookedSecond);
+});
+
 test.run();
